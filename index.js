8c678d8c7a7ac6aab9f791eb1ac1f46bb0df4363
Sabrina Fiore <sabrinafiore1992@gmail.com>
2023-03-01 18:22:38 +0100

    manage result from calculation

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 function calculateDiscount() {
-	let planAmount = ''
+
+	let planAmount = 39
 	let planValue = document.getElementById("planValue").value;
 	document.getElementById("selectPlanHtml").innerHTML = planValue;
 	planAmount = planValue
 
-	let numberOfMonth = ''
+	let numberOfMonth = 1
 	let numberOfMonthValue = document.getElementById("numberOfMonthValue").value;
 	document.getElementById("numberOfMonthHtml").innerHTML = numberOfMonthValue;
 	numberOfMonth = numberOfMonthValue
 
 	calculationPlanForMonth = planAmount * numberOfMonth
-	console.log("calculationPlanForMonth", calculationPlanForMonth)
+	// document.getElementById("calculationPlanForMonth").innerHTML = calculationPlanForMonth;
+	// console.log("calculationPlanForMonth", calculationPlanForMonth)
 
-	let priceAmount = ''
+	let priceAmount = 1
 	let priceAmountValue = document.getElementById("priceAmountValue").value;
 	document.getElementById("priceAmountHtml").innerHTML = priceAmountValue;
 	priceAmount = priceAmountValue;
 
-	let percentageFee = ''
+	let percentageFee = 0
 	let percentageFeeValue = document.getElementById("percentageFeeValue").value;
-	document.getElementById("percentageFeeHtml").innerHTML = percentageFeeValue;
+	// document.getElementById("percentageFeeHtml").innerHTML = percentageFeeValue;
 	percentageFee = percentageFeeValue;
 
-	percentageCalculator = ((priceAmount * percentageFeeValue) / 100).toFixed()
+	// console.log("priceAmount", priceAmount)
+	// console.log("percentageFeeValue", percentageFeeValue)
+
+	percentageCalculator = ((priceAmount * percentageFeeValue) / 100)
+
+	if (percentageCalculator > calculationPlanForMonth) {
+		var percentagePositiveVal = percentageCalculator
+	}
+
+	// document.getElementById("percentageCalculator").innerHTML = percentageCalculator;
 	// console.log("percentageCalculator", percentageCalculator)
 
-	let finalDiscount = percentageCalculator - calculationPlanForMonth;
-	console.log("finalDiscount", finalDiscount)
+	let discount = percentagePositiveVal - calculationPlanForMonth;
+	// console.log("finalDiscount", finalDiscount)
 
-	document.getElementById("finalDiscount").innerHTML = finalDiscount;
+	if (isNaN(discount) || discount === undefined) {
+		console.log("isNaN(discount)", isNaN(discount))
+		console.log("discount === undefined", discount === undefined)
+		document.getElementById("finalDiscount").style.display = "none";
+	} else {
+		document.getElementById("finalDiscount").style.display = "block";
+		document.getElementById("finalDiscount").innerHTML = discount;
+	}
+
 }
