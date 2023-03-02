6ef4213d483a1d3caad2695bdbea80c71abac547
Sabrina Fiore <sabrinafiore1992@gmail.com>
2023-03-02 10:29:26 +0100

    refactoring css

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function calculateDiscount() {
-
 	let planAmount = 39
 	let planValue = document.getElementById("planValue").value;
 	document.getElementById("selectPlanHtml").innerHTML = planValue;
@@ -11,8 +10,6 @@ function calculateDiscount() {
 	numberOfMonth = numberOfMonthValue
 
 	calculationPlanForMonth = planAmount * numberOfMonth
-	// document.getElementById("calculationPlanForMonth").innerHTML = calculationPlanForMonth;
-	// console.log("calculationPlanForMonth", calculationPlanForMonth)
 
 	let priceAmount = 1
 	let priceAmountValue = document.getElementById("priceAmountValue").value;
@@ -21,11 +18,8 @@ function calculateDiscount() {
 
 	let percentageFee = 0
 	let percentageFeeValue = document.getElementById("percentageFeeValue").value;
-	// document.getElementById("percentageFeeHtml").innerHTML = percentageFeeValue;
+	document.getElementById("percentageFeeHtml").innerHTML = percentageFeeValue + ' %';
 	percentageFee = percentageFeeValue;
-
-	// console.log("priceAmount", priceAmount)
-	// console.log("percentageFeeValue", percentageFeeValue)
 
 	percentageCalculator = ((priceAmount * percentageFeeValue) / 100)
 
@@ -33,19 +27,16 @@ function calculateDiscount() {
 		var percentagePositiveVal = percentageCalculator
 	}
 
-	// document.getElementById("percentageCalculator").innerHTML = percentageCalculator;
-	// console.log("percentageCalculator", percentageCalculator)
-
 	let discount = percentagePositiveVal - calculationPlanForMonth;
-	// console.log("finalDiscount", finalDiscount)
 
 	if (isNaN(discount) || discount === undefined) {
 		console.log("isNaN(discount)", isNaN(discount))
 		console.log("discount === undefined", discount === undefined)
 		document.getElementById("finalDiscount").style.display = "none";
+		document.getElementById("output-error").style.display = "block";
 	} else {
 		document.getElementById("finalDiscount").style.display = "block";
-		document.getElementById("finalDiscount").innerHTML = discount;
+		document.getElementById("output-error").style.display = "none";
+		document.getElementById("finalDiscount").innerHTML = "€ " + Math.round(discount * 100) / 1000;
 	}
-
 }
